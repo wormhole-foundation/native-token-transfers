46e413f779b0bde3ceb623079e617f6cbdf1c60c
nvsriram <nuranisri01@gmail.com>
2024-12-23 15:08:59 -0500

    nit: Remove extra space in IDL

--- solana/ts/idl/3_0_0/ts/example_native_token_transfers.ts
+++ solana/ts/idl/3_0_0/ts/example_native_token_transfers.ts
@@ -54,7 +54,7 @@ export type ExampleNativeTokenTransfers = {
           "docs": [
             "The custody account that holds tokens in locking mode and temporarily",
             "holds tokens in burning mode.",
-            "function if  the token account has already been created."
+            "function if the token account has already been created."
           ]
         },
         {
@@ -145,7 +145,7 @@ export type ExampleNativeTokenTransfers = {
               "docs": [
                 "The custody account that holds tokens in locking mode and temporarily",
                 "holds tokens in burning mode.",
-                "function if  the token account has already been created."
+                "function if the token account has already been created."
               ]
             },
             {
@@ -2512,7 +2512,7 @@ export const IDL: ExampleNativeTokenTransfers = {
           "docs": [
             "The custody account that holds tokens in locking mode and temporarily",
             "holds tokens in burning mode.",
-            "function if  the token account has already been created."
+            "function if the token account has already been created."
           ]
         },
         {
@@ -2603,7 +2603,7 @@ export const IDL: ExampleNativeTokenTransfers = {
               "docs": [
                 "The custody account that holds tokens in locking mode and temporarily",
                 "holds tokens in burning mode.",
-                "function if  the token account has already been created."
+                "function if the token account has already been created."
               ]
             },
             {
