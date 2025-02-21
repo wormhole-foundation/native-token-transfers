7b74b6beb93d8af33343bfbabcf3eed08422163d
nvsriram <nuranisri01@gmail.com>
2025-02-21 14:55:21 -0500

    Add invalid transceiver and reenable transceiver test case

--- solana/programs/example-native-token-transfers/tests/admin.rs
+++ solana/programs/example-native-token-transfers/tests/admin.rs
@@ -1,7 +1,10 @@
 #![cfg(feature = "test-sbf")]
 #![feature(type_changing_struct_update)]
 
-use example_native_token_transfers::{config::Config, error::NTTError};
+use anchor_lang::{prelude::Pubkey, system_program::System, Id};
+use example_native_token_transfers::{
+    config::Config, error::NTTError, registered_transceiver::RegisteredTransceiver,
+};
 use ntt_messages::mode::Mode;
 use solana_program_test::*;
 use solana_sdk::{instruction::InstructionError, signer::Signer, transaction::TransactionError};
@@ -30,6 +33,47 @@ async fn assert_threshold(
     assert_eq!(config_account.threshold, expected_threshold);
 }
 
+async fn assert_transceiver_id(
+    ctx: &mut ProgramTestContext,
+    test_data: &TestData,
+    transceiver: &Pubkey,
+    expected_id: u8,
+) {
+    let registered_transceiver_account: RegisteredTransceiver = ctx
+        .get_account_data_anchor(test_data.ntt.registered_transceiver(transceiver))
+        .await;
+    assert_eq!(
+        registered_transceiver_account.transceiver_address,
+        *transceiver
+    );
+    assert_eq!(registered_transceiver_account.id, expected_id);
+}
+
+#[tokio::test]
+async fn test_invalid_transceiver() {
+    let (mut ctx, test_data) = setup(Mode::Locking).await;
+
+    // try registering system program
+    let err = register_transceiver(
+        &test_data.ntt,
+        RegisterTransceiver {
+            payer: ctx.payer.pubkey(),
+            owner: test_data.program_owner.pubkey(),
+            transceiver: System::id(),
+        },
+    )
+    .submit_with_signers(&[&test_data.program_owner], &mut ctx)
+    .await
+    .unwrap_err();
+    assert_eq!(
+        err.unwrap(),
+        TransactionError::InstructionError(
+            0,
+            InstructionError::Custom(NTTError::InvalidTransceiverProgram.into())
+        )
+    );
+}
+
 #[tokio::test]
 async fn test_reregister_all_transceivers() {
     let (mut ctx, test_data) = setup(Mode::Locking).await;
@@ -43,7 +87,7 @@ async fn test_reregister_all_transceivers() {
     let num_dummy_transceivers: u8 = dummy_transceivers.len().try_into().unwrap();
 
     // register dummy transceivers
-    for transceiver in &dummy_transceivers {
+    for (idx, transceiver) in dummy_transceivers.iter().enumerate() {
         register_transceiver(
             &test_data.ntt,
             RegisterTransceiver {
@@ -55,6 +99,7 @@ async fn test_reregister_all_transceivers() {
         .submit_with_signers(&[&test_data.program_owner], &mut ctx)
         .await
         .unwrap();
+        assert_transceiver_id(&mut ctx, &test_data, transceiver, idx as u8 + 1).await;
     }
 
     // set threshold = 1 (for baked-in transceiver) + num_dummy_transceivers
@@ -98,7 +143,7 @@ async fn test_reregister_all_transceivers() {
     assert_threshold(&mut ctx, &test_data, 1).await;
 
     // reregister dummy transceiver
-    for transceiver in &dummy_transceivers {
+    for (idx, transceiver) in dummy_transceivers.iter().enumerate() {
         register_transceiver(
             &test_data.ntt,
             RegisterTransceiver {
@@ -110,6 +155,7 @@ async fn test_reregister_all_transceivers() {
         .submit_with_signers(&[&test_data.program_owner], &mut ctx)
         .await
         .unwrap();
+        assert_transceiver_id(&mut ctx, &test_data, transceiver, idx as u8 + 1).await;
         assert_threshold(&mut ctx, &test_data, 1).await;
     }
 
@@ -125,6 +171,7 @@ async fn test_reregister_all_transceivers() {
     .submit_with_signers(&[&test_data.program_owner], &mut ctx)
     .await
     .unwrap();
+    assert_transceiver_id(&mut ctx, &test_data, &example_native_token_transfers::ID, 0).await;
     assert_threshold(&mut ctx, &test_data, 1).await;
 }
 
