94dcd01ea95fbba8f81eb777012c82aebccfcd58
a <a@tuxpa.in>
2025-03-15 14:51:27 -0500

    noot

--- cli-core/dist/tag.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Platform } from "@wormhole-foundation/sdk";
-export declare function getAvailableVersions<P extends Platform>(platform: P): string[];
-export declare function getGitTagName<P extends Platform>(platform: P, version: string): string | undefined;
