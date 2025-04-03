579027bcd455acde5d099e3917e02da75b321763
nvsriram <nuranisri01@gmail.com>
2025-04-03 14:27:43 -0400

    Update LUT creation and reorder transceiver ix accounts

--- solana/programs/ntt-transceiver/src/wormhole/instructions/broadcast_peer.rs
+++ solana/programs/ntt-transceiver/src/wormhole/instructions/broadcast_peer.rs
@@ -1,6 +1,9 @@
 use crate::{peer::TransceiverPeer, wormhole::accounts::*};
 use anchor_lang::prelude::*;
-use example_native_token_transfers::config::*;
+use example_native_token_transfers::{
+    config::*,
+    wormhole_accounts::{anchor_reexports::*, *},
+};
 use ntt_messages::{chain_id::ChainId, transceivers::wormhole::WormholeTransceiverRegistration};
 
 #[derive(Accounts)]
@@ -20,13 +23,6 @@ pub struct BroadcastPeer<'info> {
     /// CHECK: initialized and written to by wormhole core bridge
     #[account(mut, seeds = [&emitter.key.to_bytes()], bump, seeds::program = wormhole_svm_definitions::solana::POST_MESSAGE_SHIM_PROGRAM_ID)]
     pub wormhole_message: UncheckedAccount<'info>,
-
-    #[account(
-        seeds = [b"emitter"],
-        bump
-    )]
-    /// CHECK: The seeds constraint ensures that this is the correct address
-    pub emitter: UncheckedAccount<'info>,
 
     pub wormhole: WormholeAccounts<'info>,
 }
@@ -52,8 +48,7 @@ pub fn broadcast_peer(ctx: Context<BroadcastPeer>, args: BroadcastPeerArgs) -> R
         &accs.wormhole,
         accs.payer.to_account_info(),
         accs.wormhole_message.to_account_info(),
-        accs.emitter.to_account_info(),
-        ctx.bumps.emitter,
+        ctx.bumps.wormhole.emitter,
         &message,
     )?;
 
--- solana/programs/ntt-transceiver/src/wormhole/instructions/release_outbound.rs
+++ solana/programs/ntt-transceiver/src/wormhole/instructions/release_outbound.rs
@@ -8,6 +8,7 @@ use example_native_token_transfers::{
     queue::outbox::OutboxItem,
     registered_transceiver::RegisteredTransceiver,
     transfer::Payload,
+    wormhole_accounts::{anchor_reexports::*, *},
 };
 use ntt_messages::{
     ntt::NativeTokenTransfer, ntt_manager::NttManagerMessage, transceiver::TransceiverMessage,
@@ -36,14 +37,6 @@ pub struct ReleaseOutbound<'info> {
     #[account(mut, seeds = [&emitter.key.to_bytes()], bump, seeds::program = wormhole_svm_definitions::solana::POST_MESSAGE_SHIM_PROGRAM_ID)]
     /// CHECK: initialized and written to by wormhole core bridge (empty, with the shim)
     pub wormhole_message: UncheckedAccount<'info>,
-
-    #[account(
-        seeds = [b"emitter"],
-        bump
-    )]
-    // TODO: do we want to put anything in here?
-    /// CHECK: wormhole uses this as the emitter address
-    pub emitter: UncheckedAccount<'info>,
 
     pub wormhole: WormholeAccounts<'info>,
 
@@ -123,8 +116,7 @@ pub fn release_outbound(ctx: Context<ReleaseOutbound>, args: ReleaseOutboundArgs
         &accs.wormhole,
         accs.payer.to_account_info(),
         accs.wormhole_message.to_account_info(),
-        accs.emitter.to_account_info(),
-        ctx.bumps.emitter,
+        ctx.bumps.wormhole.emitter,
         &message,
     )?;
 
