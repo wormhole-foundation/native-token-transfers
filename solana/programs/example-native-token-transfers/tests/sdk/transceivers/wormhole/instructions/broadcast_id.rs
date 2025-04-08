4cc18cc390b765d683fff940f1ed8d943abfb314
nvsriram <nuranisri01@gmail.com>
2025-04-07 21:57:03 -0400

    Update SDK transceiver ix's to use correct accounts

--- solana/programs/example-native-token-transfers/tests/sdk/transceivers/wormhole/instructions/broadcast_id.rs
+++ solana/programs/example-native-token-transfers/tests/sdk/transceivers/wormhole/instructions/broadcast_id.rs
@@ -1,7 +1,9 @@
+use crate::sdk::{
+    accounts::{NTTTransceiver, NTT},
+    transceivers::wormhole::accounts::wormhole::wormhole_accounts,
+};
 use anchor_lang::{prelude::*, InstructionData};
 use solana_program::instruction::Instruction;
-
-use crate::sdk::{accounts::NTT, transceivers::wormhole::accounts::wormhole::wormhole_accounts};
 
 pub struct BroadcastId {
     pub payer: Pubkey,
@@ -9,20 +11,19 @@ pub struct BroadcastId {
     pub mint: Pubkey,
 }
 
-pub fn broadcast_id(ntt: &NTT, accs: BroadcastId) -> Instruction {
-    let data = example_native_token_transfers::instruction::BroadcastWormholeId {};
+pub fn broadcast_id(ntt: &NTT, ntt_transceiver: &NTTTransceiver, accs: BroadcastId) -> Instruction {
+    let data = ntt_transceiver::instruction::BroadcastWormholeId {};
 
-    let accounts = example_native_token_transfers::accounts::BroadcastId {
+    let accounts = ntt_transceiver::accounts::BroadcastId {
         payer: accs.payer,
         config: ntt.config(),
         wormhole_message: accs.wormhole_message,
-        emitter: ntt.emitter(),
-        wormhole: wormhole_accounts(ntt),
+        wormhole: wormhole_accounts(ntt, ntt_transceiver),
         mint: accs.mint,
     };
 
     Instruction {
-        program_id: example_native_token_transfers::ID,
+        program_id: ntt_transceiver::ID,
         accounts: accounts.to_account_metas(None),
         data: data.data(),
     }
