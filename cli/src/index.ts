cda5eedd2246fdce1c33da20f6a58a3f8ccb0f9e
evgenidefi <97796468+evgeniko@users.noreply.github.com>
2025-06-27 20:45:56 +0200

    add slow flag to evm deployments with the cli to prevent issues with the local nonce

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1601,7 +1601,7 @@ forge script --via-ir script/DeployWormholeNtt.s.sol \
 --rpc-url ${rpc} \
 ${simulateArg} \
 --sig "${sig}" ${wormhole} ${token} ${effectiveRelayer} ${specialRelayer} ${decimals} ${modeUint} \
---broadcast ${verifyArgs.join(' ')} ${signerArgs} 2>&1 | tee last-run.stdout`, {
+--broadcast --slow ${verifyArgs.join(' ')} ${signerArgs} 2>&1 | tee last-run.stdout`, {
                         cwd: `${pwd}/evm`,
                         encoding: 'utf8',
                         stdio: 'inherit'
