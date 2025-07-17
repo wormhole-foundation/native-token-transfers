c0b06f3606cceec828ea90862d724cde0278ab09
nvsriram <nuranisri01@gmail.com>
2025-07-17 13:53:07 -0400

    Update `wormhole_message` to `UncheckedAccount` as its a PDA

--- solana/programs/ntt-transceiver/src/wormhole/instructions/broadcast_id.rs
+++ solana/programs/ntt-transceiver/src/wormhole/instructions/broadcast_id.rs
@@ -18,7 +18,7 @@ pub struct BroadcastId<'info> {
 
     /// CHECK: initialized and written to by wormhole core bridge
     #[account(mut, seeds = [&emitter.key.to_bytes()], bump, seeds::program = wormhole_svm_definitions::solana::POST_MESSAGE_SHIM_PROGRAM_ID)]
-    pub wormhole_message: Signer<'info>,
+    pub wormhole_message: UncheckedAccount<'info>,
 
     #[account(
         seeds = [b"emitter"],
