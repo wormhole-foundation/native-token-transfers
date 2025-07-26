4ee57769e52a70467e09b199e51aa515d43f9065
nvsriram <nuranisri01@gmail.com>
2025-07-25 20:35:48 -0400

    Split `receive_message` to `receive_message_instruction_data` and `receive_message_account`

--- solana/programs/ntt-transceiver/src/wormhole/instructions/mod.rs
+++ solana/programs/ntt-transceiver/src/wormhole/instructions/mod.rs
@@ -3,9 +3,11 @@ pub mod broadcast_id;
 pub mod broadcast_peer;
 pub mod receive_message;
 pub mod release_outbound;
+pub mod unverified_message_account;
 
 pub use admin::*;
 pub use broadcast_id::*;
 pub use broadcast_peer::*;
 pub use receive_message::*;
 pub use release_outbound::*;
+pub use unverified_message_account::*;
