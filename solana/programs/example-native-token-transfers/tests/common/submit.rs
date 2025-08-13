f4b88ac56feb4dcdc5b849192ea11912dab00a32
nvsriram <nuranisri01@gmail.com>
2025-08-13 19:55:55 -0400

    Add simulate to `Submittable` trait to help parse post message shim data

--- solana/programs/example-native-token-transfers/tests/common/submit.rs
+++ solana/programs/example-native-token-transfers/tests/common/submit.rs
@@ -1,3 +1,4 @@
+use solana_banks_interface::BanksTransactionResultWithSimulation;
 use solana_program_test::{BanksClientError, ProgramTestContext};
 use solana_sdk::{
     instruction::Instruction, signature::Keypair, signer::Signer, signers::Signers,
@@ -18,6 +19,23 @@ pub trait Submittable {
         signers: &T,
         ctx: &mut ProgramTestContext,
     ) -> Result<(), BanksClientError>;
+
+    async fn simulate(
+        self,
+        ctx: &mut ProgramTestContext,
+    ) -> Result<BanksTransactionResultWithSimulation, BanksClientError>
+    where
+        Self: Sized,
+    {
+        let no_signers: &[&Keypair] = &[];
+        self.simulate_with_signers(no_signers, ctx).await
+    }
+
+    async fn simulate_with_signers<T: Signers + ?Sized>(
+        self,
+        signers: &T,
+        ctx: &mut ProgramTestContext,
+    ) -> Result<BanksTransactionResultWithSimulation, BanksClientError>;
 }
 
 impl Submittable for Instruction {
@@ -34,6 +52,20 @@ impl Submittable for Instruction {
 
         ctx.banks_client.process_transaction(transaction).await
     }
+
+    async fn simulate_with_signers<T: Signers + ?Sized>(
+        self,
+        signers: &T,
+        ctx: &mut ProgramTestContext,
+    ) -> Result<BanksTransactionResultWithSimulation, BanksClientError> {
+        let blockhash = ctx.banks_client.get_latest_blockhash().await.unwrap();
+
+        let mut transaction = Transaction::new_with_payer(&[self], Some(&ctx.payer.pubkey()));
+        transaction.partial_sign(&[&ctx.payer], blockhash);
+        transaction.partial_sign(signers, blockhash);
+
+        ctx.banks_client.simulate_transaction(transaction).await
+    }
 }
 
 impl Submittable for Transaction {
@@ -47,5 +79,17 @@ impl Submittable for Transaction {
         self.partial_sign(&[&ctx.payer], blockhash);
         self.partial_sign(signers, blockhash);
         ctx.banks_client.process_transaction(self).await
+    }
+
+    async fn simulate_with_signers<T: Signers + ?Sized>(
+        mut self,
+        signers: &T,
+        ctx: &mut ProgramTestContext,
+    ) -> Result<BanksTransactionResultWithSimulation, BanksClientError> {
+        let blockhash = ctx.banks_client.get_latest_blockhash().await.unwrap();
+
+        self.partial_sign(&[&ctx.payer], blockhash);
+        self.partial_sign(signers, blockhash);
+        ctx.banks_client.simulate_transaction(self).await
     }
 }
