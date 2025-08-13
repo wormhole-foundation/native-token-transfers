673d35b3a307a067005de0e8086a5f63f1d0386e
nvsriram <nuranisri01@gmail.com>
2025-08-12 20:54:10 -0400

    Move `wormhole_accounts` out to sdk

--- solana/programs/example-native-token-transfers/tests/sdk/wormhole_accounts.rs
+++ solana/programs/example-native-token-transfers/tests/sdk/wormhole_accounts.rs
@@ -1,7 +1,8 @@
-use crate::sdk::accounts::{NTTTransceiver, NTT};
 use anchor_lang::prelude::*;
 use ntt_transceiver::accounts::WormholeAccounts;
 use solana_sdk::sysvar::SysvarId;
+
+use crate::sdk::accounts::{NTTTransceiver, NTT};
 
 pub fn wormhole_accounts(ntt: &NTT, ntt_transceiver: &NTTTransceiver) -> WormholeAccounts {
     WormholeAccounts {
@@ -12,7 +13,9 @@ pub fn wormhole_accounts(ntt: &NTT, ntt_transceiver: &NTTTransceiver) -> Wormhol
         system_program: System::id(),
         clock: Clock::id(),
         rent: Rent::id(),
-        transceiver: ntt_transceiver.program,
+        transceiver: ntt_transceiver.program(),
         emitter: ntt_transceiver.emitter(),
+        post_message_shim: ntt_transceiver.post_message_shim().program,
+        wormhole_post_message_shim_ea: ntt_transceiver.post_message_shim().event_authority(),
     }
 }
