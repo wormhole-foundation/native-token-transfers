307784441d9ec9a95916a394c56a95bf9a40c842
nvsriram <nuranisri01@gmail.com>
2025-08-13 20:08:12 -0400

    Update `post_vaa_helper` to use verify vaa shim version

--- solana/programs/example-native-token-transfers/tests/common/utils.rs
+++ solana/programs/example-native-token-transfers/tests/common/utils.rs
@@ -1,4 +1,4 @@
-use anchor_lang::AnchorSerialize;
+use anchor_lang::{prelude::AccountMeta, AnchorSerialize};
 use example_native_token_transfers::transfer::Payload;
 use ntt_messages::{
     chain_id::ChainId, ntt::NativeTokenTransfer, ntt_manager::NttManagerMessage,
@@ -7,10 +7,22 @@ use ntt_messages::{
 };
 use solana_program::pubkey::Pubkey;
 use solana_program_test::ProgramTestContext;
+use solana_sdk::{
+    inner_instruction::InnerInstruction, instruction::Instruction, signature::Keypair,
+    signer::Signer,
+};
 use std::sync::atomic::AtomicU64;
 use wormhole_sdk::{Address, Chain, Vaa};
 
-use crate::sdk::{accounts::NTT, instructions::post_vaa::post_vaa};
+use crate::{
+    common::submit::Submittable,
+    sdk::{
+        accounts::{NTTTransceiver, Wormhole, NTT},
+        instructions::post_vaa::{
+            get_guardian_signature, post_signatures, GUARDIAN_INDEX, GUARDIAN_SET_INDEX,
+        },
+    },
+};
 
 use super::setup::{OTHER_MANAGER, THIS_CHAIN};
 
@@ -44,19 +56,19 @@ pub fn make_transfer_message(
 }
 
 pub async fn post_vaa_helper<A: AnchorSerialize + Clone>(
-    ntt: &NTT,
+    ntt_transceiver: &NTTTransceiver,
     emitter_chain: Chain,
     emitter_address: Address,
     msg: A,
     ctx: &mut ProgramTestContext,
-) -> Pubkey {
+) -> (Pubkey, u32, Vec<u8>) {
     static I: AtomicU64 = AtomicU64::new(0);
 
     let sequence = I.fetch_add(1, std::sync::atomic::Ordering::Acquire);
 
-    let vaa = Vaa {
+    let mut vaa = Vaa {
         version: 1,
-        guardian_set_index: 0,
+        guardian_set_index: GUARDIAN_SET_INDEX,
         signatures: vec![],
         timestamp: 123232,
         nonce: 0,
@@ -66,6 +78,30 @@ pub async fn post_vaa_helper<A: AnchorSerialize + Clone>(
         consistency_level: 0,
         payload: msg,
     };
+    vaa.signatures
+        .push(get_guardian_signature(vaa.clone(), GUARDIAN_INDEX));
 
-    post_vaa(&ntt.wormhole(), ctx, vaa).await
+    let guardian_signatures = Keypair::new();
+    post_signatures(ntt_transceiver, ctx, &guardian_signatures, &vaa).await;
+
+    (
+        guardian_signatures.pubkey(),
+        GUARDIAN_SET_INDEX,
+        vaa_body(&vaa),
+    )
 }
+
+pub fn vaa_body<A: AnchorSerialize + Clone>(vaa: &Vaa<A>) -> Vec<u8> {
+    let mut bytes = Vec::new();
+    bytes.extend_from_slice(&vaa.timestamp.to_be_bytes());
+    bytes.extend_from_slice(&vaa.nonce.to_be_bytes());
+    bytes.extend_from_slice(&u16::from(vaa.emitter_chain).to_be_bytes());
+    bytes.extend_from_slice(&vaa.emitter_address.0);
+    bytes.extend_from_slice(&vaa.sequence.to_be_bytes());
+    bytes.push(vaa.consistency_level);
+    let payload_bytes = vaa.payload.try_to_vec().unwrap();
+    bytes.extend_from_slice(&payload_bytes);
+    bytes
+}
+
+
