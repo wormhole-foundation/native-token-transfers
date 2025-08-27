26fd3d4d35f0095ed11cade5d6f4fb08b37eb06e
Kevin Peters <kevin@wormholelabs.xyz>
2025-08-27 12:33:41 -0500

    axelar transceiver

---
+++ evm/ts/src/axelar.ts
@@ -0,0 +1,47 @@
+import { Chain, Network } from "@wormhole-foundation/sdk-base";
+import {
+  AxelarQueryAPI,
+  Environment,
+  EvmChain,
+} from "@axelar-network/axelarjs-sdk";
+
+export const axelarChains: Partial<Record<Chain, EvmChain>> = {
+  Sepolia: EvmChain.SEPOLIA,
+  // Monad: EvmChain.MONAD,
+  Ethereum: EvmChain.ETHEREUM,
+  // add more as needed
+};
+
+export async function getAxelarGasFee(
+  network: Network,
+  sourceChain: Chain,
+  destinationChain: Chain,
+  gasLimit: bigint
+): Promise<bigint> {
+  const axelarQueryApi = new AxelarQueryAPI({
+    environment:
+      network === "Mainnet" ? Environment.MAINNET : Environment.TESTNET,
+  });
+
+  const axelarSourceChain = axelarChains[sourceChain];
+  if (!axelarSourceChain) {
+    throw new Error(`Unsupported source chain: ${sourceChain}`);
+  }
+
+  const axelarDestinationChain = axelarChains[destinationChain];
+  if (!axelarDestinationChain) {
+    throw new Error(`Unsupported destination chain: ${destinationChain}`);
+  }
+
+  const response = await axelarQueryApi.estimateGasFee(
+    axelarSourceChain,
+    axelarDestinationChain,
+    gasLimit
+  );
+
+  if (typeof response !== "string") {
+    throw new Error(`Unexpected response type: ${typeof response}`);
+  }
+
+  return BigInt(response);
+}
