48bb12877da77cfaaeced109437d4da573af5be4
csongor <kiss.csongor.kiss@gmail.com>
2025-11-20 16:06:49 +0000

    evm: bump NTT_MANAGER_VERSION to 1.3.1

--- evm/src/Transceiver/WormholeTransceiver/WormholeTransceiver.sol
+++ evm/src/Transceiver/WormholeTransceiver/WormholeTransceiver.sol
@@ -33,7 +33,7 @@ contract WormholeTransceiver is
 {
     using BytesParsing for bytes;
 
-    string public constant WORMHOLE_TRANSCEIVER_VERSION = "1.1.0";
+    string public constant WORMHOLE_TRANSCEIVER_VERSION = "1.3.1";
 
     constructor(
         address nttManager,
