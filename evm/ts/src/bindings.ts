cb3f53fb1568fda8a601a27021ae621c33649419
Guido De Vita <24706186+dvgui@users.noreply.github.com>
2026-02-09 15:20:26 -0300

    Add EVM v2.0.0 SDK bindings (#816)
    * Add EVM v2.0.0 SDK bindings
    
    - Update NttManager version to 2.0.0
    - Update WormholeTransceiver version to 2.0.0
    - Generate TypeScript ABI bindings for v2.0.0
    - Update SDK to support v2.0.0 contract ABI
    
    * fallback rpc
    
    * fixed AbiBindings

--- evm/ts/src/bindings.ts
+++ evm/ts/src/bindings.ts
@@ -1,17 +1,20 @@
 import { Provider } from "ethers";
 
-import { _0_1_0, _1_0_0, _1_1_0 } from "./ethers-contracts/index.js";
+import { _0_1_0, _1_0_0, _1_1_0, _2_0_0 } from "./ethers-contracts/index.js";
 import { Ntt } from "@wormhole-foundation/sdk-definitions-ntt";
 
 // This is a descending list of all ABI versions the SDK is aware of.
 // We check for the first match in descending order, allowing for higher minor and patch versions
 // being used by the live contract (these are supposed to still be compatible with older ABIs).
 export const abiVersions = [
+  ["2.0.0", _2_0_0],
   ["1.1.0", _1_1_0],
   ["1.0.0", _1_0_0],
   ["0.1.0", _0_1_0],
 ] as const;
 export type AbiVersion = (typeof abiVersions)[number][0];
+
+type AbiBindings = (typeof abiVersions)[number][1];
 
 export interface NttBindings {
   NttManager: NttManagerBindings;
@@ -19,9 +22,9 @@ export interface NttBindings {
 }
 
 export namespace NttTransceiverBindings {
-  // Note: this is hardcoded to 0.1.0 so we should be warned if there are changes
-  // that would affect the interface
-  export type NttTransceiver = ReturnType<typeof _0_1_0.NttTransceiver.connect>;
+  export type NttTransceiver = ReturnType<
+    AbiBindings["NttTransceiver"]["connect"]
+  >;
 }
 
 export interface NttTransceiverBindings {
@@ -32,14 +35,14 @@ export interface NttTransceiverBindings {
 }
 
 export namespace NttManagerBindings {
-  export type NttManager = ReturnType<typeof _0_1_0.NttManager.connect>;
+  export type NttManager = ReturnType<AbiBindings["NttManager"]["connect"]>;
 }
 
 export interface NttManagerBindings {
   connect(address: string, provider: Provider): NttManagerBindings.NttManager;
 }
 
-export function loadAbiVersion(targetVersion: string) {
+export function loadAbiVersion(targetVersion: string): NttBindings {
   for (const [abiVersion, abi] of abiVersions) {
     if (Ntt.abiVersionMatches(targetVersion, abiVersion)) {
       return abi;
