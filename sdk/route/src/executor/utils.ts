01d579a3c7d2e8c59c60da5bd8bb0d748e2e1423
Kevin Peters <kevin@wormholelabs.xyz>
2026-02-10 12:01:08 -0600

    fix: prettier

--- sdk/route/src/executor/utils.ts
+++ sdk/route/src/executor/utils.ts
@@ -166,7 +166,7 @@ export function calculateReferrerFee(
 }
 
 export async function collectTransactions<N extends Network, C extends Chain>(
-  xfer: AsyncGenerator<UnsignedTransaction<N, C>>,
+  xfer: AsyncGenerator<UnsignedTransaction<N, C>>
 ): Promise<UnsignedTransaction<N, C>[]> {
   const transactions: UnsignedTransaction<N, C>[] = [];
   for await (const tx of xfer) {
