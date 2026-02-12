4d2e001b0a4e7241db7efdce870ba46624e13e38
Dave Bosley <dave@wormholelabs.xyz>
2026-02-12 10:53:41 -0700

    style: fix prettier formatting

--- evm/ts/src/index.ts
+++ evm/ts/src/index.ts
@@ -19,7 +19,7 @@ export function register(): void {
   registerProtocol(
     _platform,
     "MultiTokenNttWithExecutor",
-    EvmMultiTokenNttWithExecutor,
+    EvmMultiTokenNttWithExecutor
   );
 }
 
